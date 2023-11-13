c2d1c09f1c1609b4bf266939e32bb944df448bd5
mdapps95 <149946187+mdapps95@users.noreply.github.com>
2023-11-14 00:29:16 +0530

    Update badfeatures.java

--- badfeatures.java
+++ badfeatures.java
@@ -2,3 +2,20 @@ public void doSomething(String name, int id, String deptCode, String regNumber) 
   
     ...
 }
+
+List<String> list = new ArrayList<>();
+list.add("one");
+list.add("two");
+list.add("three");
+
+public boolean isSomething()
+{
+    if (!this.calculateSomeBooleanValue())
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
